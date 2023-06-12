a0c752bc0d21c6e9c1472f87a939a5b0cf3dce8a
Marina McPeak <mcpeakmarina@gmail.com>
2023-06-12 16:44:17 -0600

    added basic global scss and _app.tsx for React components to ensure styles are properly loaded

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../styles/global.scss';
+import React from 'react';
+import { AppProps } from 'next/app';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
