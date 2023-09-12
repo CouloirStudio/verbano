e82e4bb07c15c4462a2488a5bd8aab0da5d58f07
Caleb <calebneuf@gmail.com>
2023-09-12 08:50:28 -0600

    Added first iteration of OpenAI service
    Added the first iteration of what could be the right format for the GPT service.

---
+++ app/services/OpenAIService.ts
@@ -0,0 +1,77 @@
+import OpenAI from 'openai';
+
+class OpenAIService {
+    private readonly apiKey: string;
+    private readonly openai: OpenAI;
+
+    constructor(apiKey?: string) {
+        this.apiKey = apiKey || process.env.OPENAI_API_KEY || '';
+        if (!this.apiKey) {
+            throw new Error("API key not provided for OpenAI.");
+        }
+        this.openai = new OpenAI({
+            apiKey: this.apiKey,
+        });
+    }
+
+    /**
+     * Generates a summary report of the given text, using the text's context.
+     * @param tts the text to summarize
+     * @param context the context of the text
+     */
+    public async generateReport(tts: string, context: string): Promise<string | null> {
+        try {
+            const completion = await this.openai.chat.completions.create({
+                model: 'gpt-3.5-turbo',
+                messages: [
+                    { role: 'user', content: tts },
+                    { role: 'system', content: context }
+                ]
+            });
+            return completion.choices[0].message.content;
+        } catch (error) {
+            console.error(error);
+            throw new Error('Failed to generate report.');
+        }
+    }
+
+    /**
+     * Generates a list of tags for the given text.
+     * @param tts the text to generate tags for
+     */
+    public async generateTags(tts: string): Promise<string[] | undefined> {
+        try {
+            const completion = await this.openai.chat.completions.create({
+                model: 'gpt-3.5-turbo',
+                messages: [
+                    { role: "system", content: "You will generate a list of 4 content tags related to the following text split by commas:" },
+                    { role: "user", content: tts },
+                ],
+            });
+            return completion.choices[0].message.content?.split(",");
+        } catch (error) {
+            console.error(error);
+            throw new Error('Failed to generate tags.');
+        }
+    }
+
+    /**
+     * Generates a title for the given text.
+     * @param tts the text to generate a title for
+     */
+    public async generateTitle(tts: string): Promise<string | null> {
+        try {
+            const completion = await this.openai.chat.completions.create({
+                model: 'gpt-3.5-turbo',
+                messages: [
+                    { role: "system", content: "You will generate a title for the following text:" },
+                    { role: "user", content: tts },
+                ],
+            });
+            return completion.choices[0].message.content;
+        } catch (error) {
+            console.error(error);
+            throw new Error('Failed to generate title.');
+        }
+    }
+}
