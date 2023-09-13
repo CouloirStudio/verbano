dae5c0decf7d6c4bc44edab11e8b8b4ac91787f5
Janel Semple <105823477+janelsemple@users.noreply.github.com>
2023-09-13 13:19:58 -0600

    Does not build :(

--- app/components/Recorder/index.tsx
+++ app/components/Recorder/index.tsx
@@ -4,6 +4,9 @@ import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 import { useRecorderContext, RecorderProvider } from '../../contexts/RecorderContext';
 
+import RecordRTC, {invokeSaveAsDialog} from 'recordrtc';
+
+
 // GraphQL mutation to create a new note entry with an audio location
 const CREATE_NOTE_MUTATION = gql`
   mutation CreateNote($audioLocation: String!) {
@@ -17,33 +20,54 @@ const CREATE_NOTE_MUTATION = gql`
 `;
 
 const Recorder: React.FC = () => {
+
     // Retrieve recording state and control functions from the context
     const { isRecording, startRecording, stopRecording } = useRecorderContext();
 
     // Apollo Client hook to call the CREATE_NOTE_MUTATION
     // const [createNote] = useMutation(CREATE_NOTE_MUTATION);
 
-    const toggleRecording = async () => {
-        // If already recording, stop and save the audio
-        if (isRecording) {
-            stopRecording();
+        const toggleRecording = async () => {
 
-            // Mock URL for the audio location (replace with logic to upload to AWS S3)
-            const mockS3Url = "https://aws-s3-bucket/your-recording-file.mp3";
-            /*
-            try {
-                // Call the mutation to create a new note with the audio URL
-                const { data } = await createNote({ variables: { audioLocation: mockS3Url } });
-                console.log('New note created:', data.addNote);
-            } catch (error) {
-                console.error('Error creating note:', error);
+            navigator.mediaDevices.getUserMedia({
+                audio: true
+            }).then(async function(stream) {
+                let recorder = new RecordRTC(stream, {
+                    type: 'audio'
+                });
+
+            // If already recording, stop and save the audio
+            if (isRecording) {
+
+                recorder.stopRecording(function() {
+                    let blob = recorder.getBlob();
+                    invokeSaveAsDialog(blob);
+                });
+
+                stopRecording();
+
+
+                // Mock URL for the audio location (replace with logic to upload to AWS S3)
+                const mockS3Url = "https://aws-s3-bucket/your-recording-file.mp3";
+                /*
+                try {
+                    // Call the mutation to create a new note with the audio URL
+                    const { data } = await createNote({ variables: { audioLocation: mockS3Url } });
+                    console.log('New note created:', data.addNote);
+                } catch (error) {
+                    console.error('Error creating note:', error);
+                }
+                */
+            } else {
+                // If not recording, start
+                recorder.startRecording();
+                startRecording();
             }
-            */
-        } else {
-            // If not recording, start
-            startRecording();
-        }
+        });
     };
+
+
+
 
     return (
         <div className={styles.recorder}>
