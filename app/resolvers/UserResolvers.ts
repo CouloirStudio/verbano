a0d4d4b5841c163b1947d091c37c6eac9c92fb12
Caleb <calebneuf@gmail.com>
2023-09-13 22:36:39 -0600

    Adjusted UserSchema and UserResolvers
    Wrote new resolvers, and adjusted the schema

--- app/resolvers/UserResolvers.ts
+++ app/resolvers/UserResolvers.ts
@@ -1,9 +1,66 @@
-const UserResolvers = {
-  Query: {
-    users: () => {
-      // Return a list of users
-    }
-  }
-};
+import User from '../models/User';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export default UserResolvers;
+module.exports = {
+    Query: {
+        user: (_: any, {id}: any) => User.findById(id)
+    },
+
+    Mutation: {
+      async registerUser(_: any, {registerInput: {email, password, firstName, lastName}}: any) {
+
+        // See if user exists
+        const oldUser = await User.findOne({email});
+        if (oldUser) {
+            throw new Error('User already exists');
+        }
+
+        // Encrypt password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create user
+        const newUser = new User({
+            email: email.toLowerCase(),
+            password: hashedPassword,
+            firstName,
+            lastName
+
+        });
+
+        // Save user
+
+
+        newUser.token = jwt.sign({userId: newUser._id, email}, "CHANGE_ME_SECRET", {expiresIn: '1h'});
+
+        const result = await newUser.save();
+
+        // Return user
+        return {
+            id: result.id,
+            ...result.toObject()
+        }
+      },
+      async loginUser(_: any, {loginInput: {email, password}}: any) {
+          const user = await User.findOne({email});
+
+            if (!user) {
+                throw new Error('User does not exist');
+            }
+
+            const valid = await bcrypt.compare(password, user.password);
+
+            if (!valid) {
+                throw new Error('Invalid password');
+            }
+
+            user.token = jwt.sign({userId: user._id, email}, "CHANGE_ME_SECRET", {expiresIn: '1h'});
+
+            return {
+                id: user.id,
+                ...user.toObject()
+            }
+
+      }
+    }
+}
--- app/schema/UserSchema.ts
+++ app/schema/UserSchema.ts
@@ -1,15 +1,44 @@
-import { gql } from 'apollo-server-express';
+import {gql} from 'apollo-server-express';
 
-const UserSchema = gql`
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-  }
+const User = gql`
+    type User {
+        id: ID!
+        email: String!
+        password: String!
+        firstName: String!
+        lastName: String!
+        profilePicture: String
+        registeredIP: String
+        lastLoginIP: String
+        settings: UserSettings
+        token: String
+    }
 
-  type Query {
-    users: [User!]!
-  }
+    type UserSettings {
+        darkMode: Boolean
+        notifications: Boolean
+    }
+
+    input RegisterInput {
+        email: String!
+        password: String!
+        firstName: String!
+        lastName: String!
+    }
+
+    input LoginInput {
+        email: String!
+        password: String!
+    }
+
+    type Query {
+        user(id: ID!): User
+    }
+
+    type Mutation {
+        registerUser(registerInput: RegisterInput): User
+        loginUser(loginInput: LoginInput): User
+    }
 `;
 
-export default UserSchema;
+export default User;
