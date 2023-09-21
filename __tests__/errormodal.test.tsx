f711fac6d4f04967d9b6d20b27a1780725d08e56
Janel Semple <105823477+janelsemple@users.noreply.github.com>
2023-09-21 10:44:39 -0600

    Added comments to the tests.

--- __tests__/errormodal.test.tsx
+++ __tests__/errormodal.test.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react';
-import { render, screen } from '@testing-library/react';
-import ErrorModal from '@/app/components/ErrorModal';
-import '@testing-library/jest-dom';
-import {
-  ErrorModalContextProvider,
-  useErrorModalContext,
-} from '@/app/contexts/ErrorModalContext';
+import React, { useEffect } from "react";
+import { render, screen } from "@testing-library/react";
+import ErrorModal from "@/app/components/ErrorModal";
+import "@testing-library/jest-dom";
+import { ErrorModalContextProvider, useErrorModalContext } from "@/app/contexts/ErrorModalContext";
 
+// This is a simple set of tests for the ErrorModal component and the accompanying ErrorModalContext.
 describe('ErrorModal Component', () => {
   beforeEach(() => {});
 
+  // Testing Component to update the ErrorModalContext using hooks from within the provider.
+  // Values are hardcoded as there is only one other alternative to the default values.
   const TestingComponent = () => {
     const { setIsError, setErrorMessage } = useErrorModalContext();
     useEffect(() => {
@@ -19,6 +19,7 @@ describe('ErrorModal Component', () => {
     return <div></div>;
   };
 
+  // Simply renders the component in it's default state and checks that it did not render.
   it('Does not render when isError is false', () => {
     render(
       <ErrorModalContextProvider>
@@ -28,6 +29,7 @@ describe('ErrorModal Component', () => {
     expect(screen.queryByText('Dismiss')).not.toBeInTheDocument();
   });
 
+  // Renders the component with the testing component, which updates the values and triggers the render.
   it('renders the modal when isError is true', () => {
     render(
       <ErrorModalContextProvider>
