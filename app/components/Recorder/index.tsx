6f5bfbc5717f1ac8d1d4306e114f7ad5713e3035
Adrian <Adrian.tomin@gmail.com>
2023-09-20 21:01:13 -0600

    Created a set of tests for the Recorder component

--- app/components/Recorder/index.tsx
+++ app/components/Recorder/index.tsx
@@ -38,29 +38,32 @@ const Recorder: React.FC = () => {
           type: 'audio',
         });
         // updating recorder context and starting the recorder.
-        startRecording();
+        await startRecording();
         recorder.startRecording();
         setCurrentRecorder(recorder);
       } catch (error) {
         // Update the error context so that the modal is displayed
         setErrorMessage(
-          'We encountered an error while accessing the microphone: please ensure your microphone is connected and allowed in the browser. See our troubleshooting guide for details: INSERT LINK HERE',
+          'We encountered an error while accessing the microphone: ' +
+            'please ensure your microphone is connected and allowed in the browser. ' +
+            'See our troubleshooting guide for details: INSERT LINK HERE',
         );
         // this is what will make the error modal appear.
         setIsError(true);
         console.error('Error accessing the microphone:' + error);
+
       }
     } else {
       // if current recorder is not null, then stop recording, this is used to be type-safe.
       if (currentRecorder) {
-        currentRecorder.stopRecording(function () {
+        currentRecorder.stopRecording(() => {
           stopRecording();
           // Stop the stream when recording stops
           clearStreams();
-          // Get the blob and save it to the context so it can be accessed by other components
+          // Get the blob and save it to the context, so it can be accessed by other components
           const blob = currentRecorder.getBlob();
           setAudioBlob(blob);
-          // This is temporary, but will save the audio to your browser so you know that it is working properly.
+          // This is temporary, but will save the audio to your browser, so you know that it is working properly.
           invokeSaveAsDialog(blob);
           // Destroy the recorder and set the context for it to null.
           currentRecorder.destroy();
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,6 @@
 require('@testing-library/jest-dom');
+
+// Mock the navigator.mediaDevices object with a mock for getUserMedia
+navigator.mediaDevices = {
+    getUserMedia: jest.fn(),
+};
