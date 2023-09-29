5a66a3092dcea33fbf489c8b5eb11853f62eb8fb
Adrian <Adrian.tomin@gmail.com>
2023-09-29 14:48:04 -0600

    Created settings page, will probably refactor so the link from the home page will land on a specific sub page instead

---
+++ pages/settings.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { SettingsSidebar } from "@/app/components/SettingsSidebar";
+
+const Settings = () => {
+    return (
+        <>
+            <SettingsSidebar />
+        </>
+    )
+}
+
+export default Settings;
