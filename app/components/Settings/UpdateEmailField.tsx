96f3514eea6e8c733e7cbcc27d624d5ed7de4592
Adrian <Adrian.tomin@gmail.com>
2023-09-30 21:42:42 -0600

    Utilized the custom input field component found in the login page in the settings page

---
+++ app/components/Settings/UpdateEmailField.tsx
@@ -0,0 +1,23 @@
+import React, {ChangeEvent} from 'react';
+import InputField from '@/app/components/Login/InputField';
+import {AiOutlineMail} from "react-icons/ai";
+
+interface EmailInputProps {
+    value: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const UpdateEmailField: React.FC<EmailInputProps> = ({value, onChange}) => {
+    return (
+        <InputField label="Update Email" icon={<AiOutlineMail/>}>
+            <input
+                type="text"
+                value={value}
+                onChange={onChange}
+                required={true}
+            />
+        </InputField>
+    );
+};
+
+export default UpdateEmailField;
