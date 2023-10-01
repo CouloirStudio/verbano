0fe590824cf181a08f657ee374873cb5e34d505d
Adrian <Adrian.tomin@gmail.com>
2023-09-30 21:39:25 -0600

    Added UpdateFullName mutation

--- app/middleware/mutations.ts
+++ app/middleware/mutations.ts
@@ -35,3 +35,13 @@ export const SIGNUP_MUTATION = gql`
     }
   }
 `;
+
+export const UPDATE_FULL_NAME_MUTATION = gql`
+  mutation UpdateFullName($email: String!, $firstName: String!, $lastName: String!) {
+    updateFullName(email: $email, firstName: $firstName, lastName: $lastName) {
+      email
+      firstName
+      lastName
+    }
+  }
+`;
