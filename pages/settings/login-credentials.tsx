43bac1273cc34e888ee82353de944cbaf862a001
Adrian <Adrian.tomin@gmail.com>
2023-10-02 18:00:39 -0600

    Added boilerplate code to start on updating the password

---
+++ pages/settings/login-credentials.tsx
@@ -0,0 +1,14 @@
+/*
+<UpdatePasswordField
+                value={''}
+                onChange={(e) => setCurrentPassword(e.target.value)}
+                text="Current Password"
+            />
+            <UpdatePasswordField
+                value={''}
+                onChange={(e) => setNewPassword(e.target.value)}
+                text="New Password"
+            />
+ */
+
+//TODO implement page for updating password
