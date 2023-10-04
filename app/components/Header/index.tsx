2edd87c81a63d97f099e3a746603d6355b2e20df
Adrian <Adrian.tomin@gmail.com>
2023-10-04 12:34:08 -0600

    Added placeholder button for quick navigation between settings and the homepage for dev. Will have to adjust css but can be used as a placeholder for now

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './header.module.scss';
 import dynamic from 'next/dynamic';
 import { RecorderProvider } from '../../contexts/RecorderContext';
+import Link from "next/link";
 
 const Recorder = dynamic(() => import('../../components/Recorder'), {
   ssr: false,
@@ -13,6 +14,11 @@ function Header() {
       <RecorderProvider>
         <Recorder />
       </RecorderProvider>
+        <div className={styles.linkContainer}>
+            <Link href="/settings/profile">
+                <span className={styles.link}>Settings</span>
+            </Link>
+        </div>
     </div>
   );
 }
