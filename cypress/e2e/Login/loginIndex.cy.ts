5f8e79a93c97b97b07d989c3e2abb8f17faebc2e
Caleb <calebneuf@gmail.com>
2023-10-03 22:52:03 -0600

    Added more test cases

--- cypress/e2e/Login/loginIndex.cy.ts
+++ cypress/e2e/Login/loginIndex.cy.ts
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('Email/Pass Login', () => {
   it('loads correctly', () => {
     cy.visit('localhost:3000/login', {
       timeout: 10000,
@@ -17,10 +17,48 @@ describe('template spec', () => {
     cy.get('#password')
       .type('password').should('have.value', 'password');
 
-    cy.get('#loginButton').click().next().should('contain', 'Welcome');
+    cy.get('#loginButton').click();
+    cy.location('pathname').should('eq', '/');
   })
 
-  it('errors on invalid credentials', () => {
+  it('errors on invalid email', () => {
+    cy.visit('localhost:3000/login', {
+      timeout: 10000,
+      failOnStatusCode: true
+    });
 
+    cy.get('#email')
+      .type('wrong@gmail.com').should('have.value', 'wrong@gmail.com');
+    cy.get('#password')
+      .type('password').should('have.value', 'password');
+
+    cy.get('#loginButton').click();
+    cy.contains('Woopsies!');
+  })
+
+  it('errors on invalid password', () => {
+    cy.visit('localhost:3000/login', {
+      timeout: 10000,
+      failOnStatusCode: true
+    });
+
+    cy.get('#email')
+      .type('test@gmail.com').should('have.value', 'test@gmail.com');
+    cy.get('#password')
+      .type('wrong').should('have.value', 'wrong');
+
+    cy.get('#loginButton').click();
+    cy.contains('Woopsies!');
+  })
+})
+
+describe('Google Login', () => {
+  it('Google button loads', () => {
+    cy.visit('localhost:3000/login', {
+      timeout: 10000,
+      failOnStatusCode: true
+    });
+
+    cy.contains("Login with Google");
   })
 })
