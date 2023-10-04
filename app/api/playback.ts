cf62bf623a51df0e59dcf17217844e07bec39fa8
Janel Semple <105823477+janelsemple@users.noreply.github.com>
2023-10-03 21:36:54 -0600

    added some more comments/error handling. Started building up usePlaybackManager.

--- app/api/playback.ts
+++ app/api/playback.ts
@@ -1,24 +1,31 @@
+// The purpose of this class is to abstract the usage of the HTMLAudioElement API
+// and also handle any errors that may be thrown.
+
 export class AudioPlayer {
   private static instance: AudioPlayer | undefined;
   public audio: HTMLAudioElement | undefined;
 
   private constructor() {}
 
-  public getAudioPlayer() {
+  // implementing singleton pattern so there will never be two audio players going at once.
+  public static getAudioPlayer() {
     if (!AudioPlayer.instance) {
       AudioPlayer.instance = new AudioPlayer();
     }
     return AudioPlayer.instance;
   }
 
-  //initializes the audio element, and waits for the data to be loaded.
-  public initializeAudioPlayer(src: string) {
+  // Loads the audio element using a blob url and waits for the data to be loaded.
+  public loadAudioPlayer(src: string) {
     return new Promise<void>((resolve) => {
       this.audio = new Audio(src);
+      // Making the caller of this method wait for the canPlayThrough event.
+      // Which is fired when all the audio is loaded.
       const OnReady = () => {
+        console.log('audio ready');
         resolve();
       };
-      this.audio.addEventListener('canPlayThrough', OnReady);
+      this.audio.addEventListener('canplaythrough', OnReady);
     });
   }
 
@@ -32,6 +39,10 @@ export class AudioPlayer {
   };
 
   public pauseAudioPlayer() {
-    this.audio?.pause();
+    if (!this.audio) {
+      throw new Error('audio player not initialized.');
+    } else if (this.audio) {
+      this.audio.pause();
+    }
   }
 }
