0dcb865e4c7e69a24ac11527e1bc413c4de9b033
Janel Semple <105823477+janelsemple@users.noreply.github.com>
2023-10-09 10:35:44 -0600

    Put playback button next to recorder button for now for testing purposes.

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './header.module.scss';
 import dynamic from 'next/dynamic';
 import { RecorderProvider } from '../../contexts/RecorderContext'; // Ensure the correct path
+import { Playback } from '@/app/components/Playback';
 
 const Recorder = dynamic(() => import('../../components/Recorder'), {
   ssr: false,
@@ -11,6 +12,7 @@ function Header() {
   return (
     <div className={styles.header}>
       <RecorderProvider>
+        <Playback />
         <Recorder />
       </RecorderProvider>
     </div>
