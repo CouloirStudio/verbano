d1e21929bc971759092f6f36e8867442dd476843
Janel Semple <105823477+janelsemple@users.noreply.github.com>
2023-10-09 20:07:39 -0600

    Removed playback from header.

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './header.module.scss';
 import dynamic from 'next/dynamic';
-import { RecorderProvider } from '../../contexts/RecorderContext'; // Ensure the correct path
-import { Playback } from '@/app/components/Playback';
-import Link from "next/link";
+import { RecorderProvider } from '../../contexts/RecorderContext';
+import Link from 'next/link';
+
 const Recorder = dynamic(() => import('../../components/Recorder'), {
   ssr: false,
 });
@@ -12,14 +12,13 @@ function Header() {
   return (
     <div className={styles.header}>
       <RecorderProvider>
-        <Playback />
         <Recorder />
       </RecorderProvider>
-        <div className={styles.linkContainer}>
-            <Link href="/settings/profile">
-                <span className={styles.link}>Settings</span>
-            </Link>
-        </div>
+      <div className={styles.linkContainer}>
+        <Link href="/settings/profile">
+          <span className={styles.link}>Settings</span>
+        </Link>
+      </div>
     </div>
   );
 }
