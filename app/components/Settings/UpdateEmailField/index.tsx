7f4636258e5e0bfd7caf00e68cb41c7182966416
Adrian <Adrian.tomin@gmail.com>
2023-10-12 12:49:48 -0600

    Added documentation to the UpdateEmailField

--- app/components/Settings/UpdateEmailField/index.tsx
+++ app/components/Settings/UpdateEmailField/index.tsx
@@ -2,6 +2,25 @@ import React, { ChangeEvent } from 'react';
 import InputField from '@/app/components/Login/InputField';
 import { AiOutlineMail } from 'react-icons/ai';
 
+/**
+ * `UpdateEmailField` is a React functional component that provides an input field for updating a user's email address.
+ *
+ * @remarks
+ * This component is designed for use in forms where a user can update their email address.
+ *
+ * @param props - The component's props.
+ * @param props.value - The current value of the email input field.
+ * @param props.label - The label for the email input field.
+ * @param props.onChange - A function to handle changes to the email input field.
+ *
+ * @example
+ * ```tsx
+ * <UpdateEmailField value={emailValue} label="New Email" onChange={handleEmailChange} />
+ * ```
+ *
+ * @see {@link https://react-icons.github.io/react-icons/ | react-icons} for including icons.
+ *
+ */
 interface EmailInputProps {
   value: string;
   label: string
