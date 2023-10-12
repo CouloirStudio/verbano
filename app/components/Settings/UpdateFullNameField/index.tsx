cbea31e5aaa1251c769fe21c3fa702b9d70de326
Adrian <Adrian.tomin@gmail.com>
2023-10-12 12:49:59 -0600

    Added documentation to the UpdateFullNameField

--- app/components/Settings/UpdateFullNameField/index.tsx
+++ app/components/Settings/UpdateFullNameField/index.tsx
@@ -10,7 +10,34 @@ interface FullNameInputProps {
   onFirstNameChange: (e: ChangeEvent<HTMLInputElement>) => void;
   onLastNameChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
-
+/**
+ * `UpdateFullName` is a React functional component that provides input fields for updating a user's first name and last name with customizable labels.
+ *
+ * @remarks
+ * This component is designed for use in forms where a user can update their full name. It allows you to customize the labels for the input fields.
+ *
+ * @param props - The component's props.
+ * @param props.firstName - The current value of the first name input field.
+ * @param props.lastName - The current value of the last name input field.
+ * @param props.firstLabel - The label for the first name input field.
+ * @param props.secondLabel - The label for the last name input field.
+ * @param props.onFirstNameChange - A function to handle changes to the first name input field.
+ * @param props.onLastNameChange - A function to handle changes to the last name input field.
+ *
+ * @example
+ * ```tsx
+ * <UpdateFullName
+ *   firstName={firstNameValue}
+ *   lastName={lastNameValue}
+ *   firstLabel="First Name"
+ *   secondLabel="Last Name"
+ *   onFirstNameChange={handleFirstNameChange}
+ *   onLastNameChange={handleLastNameChange}
+ * />
+ * ```
+ *
+ * @see {@link https://react-icons.github.io/react-icons/ | react-icons} for including icons.
+ */
 const UpdateFullName: React.FC<FullNameInputProps> = ({
   firstName,
   lastName,
@@ -24,6 +51,7 @@ const UpdateFullName: React.FC<FullNameInputProps> = ({
 }) => {
   return (
     <>
+      {/* InputField with label and icon for first name */}
       <InputField label={firstLabel} icon={<AiOutlineUser />}>
         <input
           type="text"
@@ -32,6 +60,8 @@ const UpdateFullName: React.FC<FullNameInputProps> = ({
           required
         />
       </InputField>
+
+      {/* InputField with label and icon for last name */}
       <InputField label={secondLabel} icon={<AiOutlineUser />}>
         <input
           type="text"
