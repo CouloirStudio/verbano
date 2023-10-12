b7c08c42d34c33fc2659059e699b529a9c7712e3
Caleb <calebneuf@gmail.com>
2023-10-12 12:05:39 -0600

    Added more routing tests
    Testing accessing /login when logged in, same with /register, and the use of /logout

--- cypress/e2e/Authentication/authRedirect.cy.ts
+++ cypress/e2e/Authentication/authRedirect.cy.ts
@@ -7,4 +7,49 @@ describe('Authentication Redirection', () => {
 
     cy.location('pathname').should('eq', '/login');
   });
+
+  it('cant access /login when logged in', () => {
+    cy.login();
+    cy.visit('localhost:3000/login', {
+      timeout: 10000,
+      failOnStatusCode: true
+    });
+
+    cy.location('pathname').should('eq', '/');
+  });
+
+  it('cant access /register when logged in', () => {
+    cy.login();
+    cy.visit('localhost:3000/register', {
+      timeout: 10000,
+      failOnStatusCode: true
+    });
+
+    cy.location('pathname').should('eq', '/');
+  });
+
+  it('accessing /logout when logged in redirects to /login', () => {
+    cy.login();
+    cy.visit('localhost:3000/logout', {
+      timeout: 10000,
+      failOnStatusCode: true
+    });
+
+    cy.location('pathname').should('eq', '/login');
+  });
+
+  it('logging out, then accessing the dashboard redirects to /login', () => {
+    cy.login();
+    cy.visit('localhost:3000/logout', {
+      timeout: 10000,
+      failOnStatusCode: true
+    });
+
+    cy.visit('localhost:3000/', {
+      timeout: 10000,
+      failOnStatusCode: true
+    });
+
+    cy.location('pathname').should('eq', '/login');
+  });
 });
