9bbe2343f8b932927f50eff5212da86e21cec757
Marina McPeak <mcpeakmarina@gmail.com>
2023-10-12 14:15:43 -0600

    Revert "Feature/routing authentication"

--- server/server.ts
+++ server/server.ts
@@ -19,7 +19,7 @@ const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 
-export function createApp(mockMiddleware?: any) {
+export function createApp() {
   const app = express();
 
   app.use(
@@ -30,25 +30,6 @@ export function createApp(mockMiddleware?: any) {
       saveUninitialized: false,
     }),
   );
-
-  const corsOptions = {
-    origin: ['http://localhost:3000/graphql', 'http://localhost:3000'],
-    credentials: true,
-  }
-  app.use(cors(corsOptions));
-
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(json());
-
-
-  // if test environment, use mock middleware
-  if (process.env.NODE_ENV === 'test') {
-    if (mockMiddleware) {
-      app.use(mockMiddleware);
-    }
-    app.use('/auth-check', authCheckTestMiddleware);
-  }
 
   app.get(
     '/auth/google',
@@ -66,65 +47,20 @@ export function createApp(mockMiddleware?: any) {
       res.redirect('/');
     },
   );
-  app.get('/logout', handleLogout);
 
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  const corsOptions = {
+    origin: '*',
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+  app.use(json());
   app.use('/audio', audioRoutes);
-
 
   return app;
 }
-
-function isAuthenticated(req: any, res: any, next: any) {
-  // If test environment, skip authentication
-  if (process.env.NODE_ENV === 'test') {
-    return next();
-  }
-
-  // If user is authenticated
-  if (req.user) {
-    // Prevent authenticated users from accessing /login or /register
-    if (['/login', '/register'].includes(req.path)) {
-      return res.redirect('/'); // Redirect to home or another suitable page
-    }
-    return next();
-  }
-
-  // Allow certain paths without authentication
-  if (['/graphql'].includes(req.path) || req.path.startsWith('/_next')) {
-    return next();
-  }
-
-  // If not authenticated and not already on /login or /register, redirect to /login
-  if (!['/login', '/register'].includes(req.path)) {
-    res.redirect('/login');
-  } else {
-    next();  // If already on /login or /register, continue without redirecting
-  }
-}
-
-async function handleLogout(req: any, res: any) {
-  try {
-    const result = await UserMutations.logout(null, null, {req});
-    if (result) {
-      res.redirect('/');
-    } else {
-      res.status(500).send('Failed to logout');
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error during logout');
-  }
-}
-
-
-function authCheckTestMiddleware(req: any, res: any, next: any) {
-  if (req.user) {
-    res.status(200).send('Authenticated');
-  } else {
-    res.status(401).send('Unauthorized');
-  }
-}
-
 
 export async function startApolloServer(
   app: express.Express,
@@ -158,12 +94,9 @@ export async function startApolloServer(
   await server.start();
   server.applyMiddleware({ app, cors: false });
 
-  app.use(isAuthenticated);
-
   app.all('*', (req, res) => {
     return handle(req, res);
   });
-
 
   const actualPort = testPort !== undefined ? testPort : port;
 
