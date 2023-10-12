9b9f926e7b9a75188e3eef1f2b45eb01510264a2
Janel Semple <105823477+janelsemple@users.noreply.github.com>
2023-10-12 11:41:41 -0600

    Fixed warnings

--- app/components/Playback/index.ts
+++ app/components/Playback/index.ts
@@ -1,3 +1,3 @@
 export { default as Playback } from './Playback';
 export { default as PlaybackButton } from './PlaybackButton';
-export type { PlaybackState, PlaybackButtonProps } from './PlaybackButton';
+export type { PlaybackButtonProps } from './PlaybackButton';
