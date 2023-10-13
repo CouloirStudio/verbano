9cfc6bc1e71fd237c0b0748f1fcce1e3c07d3dc9
Caleb <calebneuf@gmail.com>
2023-10-12 22:35:05 -0600

    Changed : any type
    change any type to proper types

--- app/components/UserProvider/index.tsx
+++ app/components/UserProvider/index.tsx
@@ -1,4 +1,4 @@
-import React, {createContext, useContext} from 'react';
+import React, {createContext, ReactNode, useContext} from 'react';
 import {CURRENT_USER_QUERY} from "../../../app/graphql/queries/getUsers";
 import {IUser} from "@/app/models/User";
 import {useQuery} from "@apollo/react-hooks";
@@ -9,7 +9,11 @@ export const useUser = () => {
   return useContext(UserContext);
 };
 
-export const UserProvider = ({children}: any) => {
+interface UserProviderProps {
+  children: ReactNode;
+}
+
+export const UserProvider: React.FC<UserProviderProps> = ({children}) => {
   const {loading, error, data} = useQuery(CURRENT_USER_QUERY);
 
   if (loading) return <p>Loading...</p>;
