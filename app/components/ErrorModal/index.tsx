00abf51d150f97f84e95ce9392ee38b8f3a3cf3b
Caleb <calebneuf@gmail.com>
2023-10-17 09:29:06 -0600

    Updated error modal to work with new default modal
    The error modal will update iwth the default modals new system

--- app/components/ErrorModal/index.tsx
+++ app/components/ErrorModal/index.tsx
@@ -1,9 +1,14 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import styles from './errormodal.module.scss';
-import { useErrorModalContext } from '../../contexts/ErrorModalContext';
+import {useErrorModalContext} from '../../contexts/ErrorModalContext';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import {AiFillQuestionCircle} from "react-icons/ai";
+import DefaultModal from "../DefaultModal";
 
 const ErrorModal = () => {
-  const { isError, errorMessage, setIsError } = useErrorModalContext();
+  const {isError, errorMessage, setIsError} = useErrorModalContext();
 
   // Set to false on unmount.
   useEffect(() => {
@@ -12,30 +17,35 @@ const ErrorModal = () => {
     };
   }, [setIsError]);
 
-  // dismiss the modal by setting isError to false.
   const dismissModal = () => {
     setIsError(false);
   };
 
+  const modalBody = (
+    <Box className={styles.ErrorModal}>
+      <AiFillQuestionCircle className={styles.icon}/>
+      <Typography id="errorTitle" variant="h6" className={styles.title}>
+        WOOPS!
+      </Typography>
+      <Typography id="errorMessage" className={styles['modal-error']}>
+        {errorMessage}
+      </Typography>
+      <Button variant="text" onClick={dismissModal}>
+        Dismiss
+      </Button>
+    </Box>
+  );
+
   return (
-    <div>
-      {/* Conditionally render the overlay and modal, if isError is set to true, it will appear. */}
-      {isError && (
-        <div className={styles.ErrorModalOverlay}>
-          <div className={styles.ErrorModal}>
-            <span id={'errorTitle'} className={styles.title}>
-              Woopsies!
-            </span>
-            <p id={'errorMessage'} className={styles['modal-error']}>
-              {errorMessage}
-            </p>
-            <button className={styles.button} onClick={dismissModal}>
-              Dismiss
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
+
+    <DefaultModal
+      isOpen={isError}
+      onClose={dismissModal}
+      title="WOOPS!"
+      className={`${styles.ErrorModal}`} // You can append or override the default class here
+    >
+      {modalBody}
+    </DefaultModal>
   );
 };
 
