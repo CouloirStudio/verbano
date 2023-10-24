c72a75a6e2c5dec4a39296c5258af46eb68f766d
Caleb <calebneuf@gmail.com>
2023-10-23 20:44:42 -0600

    Dropdown avatar menu

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './header.module.scss';
 import dynamic from 'next/dynamic';
 import { RecorderProvider } from '../../contexts/RecorderContext';
 import Link from 'next/link';
-import { Avatar } from '@mui/material';
+import { Avatar, Divider, Menu, MenuItem } from '@mui/material';
 import { useUser } from '@/app/components/UserProvider';
 import { deepPurple } from '@mui/material/colors';
+import { BiLogOut } from 'react-icons/bi';
+import { IoSettingsOutline } from 'react-icons/io5';
 
 const Recorder = dynamic(() => import('../../components/Recorder'), {
   ssr: false,
@@ -13,6 +15,18 @@ const Recorder = dynamic(() => import('../../components/Recorder'), {
 
 function Header() {
   const currentUser = useUser();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   if (!currentUser) return null;
 
   return (
@@ -21,11 +35,30 @@ function Header() {
         <Recorder />
       </RecorderProvider>
       <div className={styles.linkContainer}>
-        <Link href="/settings/profile">
-          <Avatar sx={{ bgcolor: deepPurple[500] }}>
-            {currentUser.firstName?.substring(0, 1)}
-          </Avatar>
-        </Link>
+        <Avatar onClick={handleMenuOpen} sx={{ bgcolor: deepPurple[500] }}>
+          {currentUser.firstName?.substring(0, 1)}
+        </Avatar>
+        <Menu anchorEl={anchorEl} open={open} onClose={handleMenuClose}>
+          <Link href="/settings/profile">
+            <MenuItem sx={{ gap: '5px' }} onClick={handleMenuClose}>
+              <IoSettingsOutline />
+              Settings
+            </MenuItem>
+          </Link>
+          <Divider />
+          <MenuItem
+            sx={{ gap: '5px' }}
+            onClick={() => {
+              handleMenuClose();
+
+              //route to /logout
+              window.location.replace('/logout');
+            }}
+          >
+            <BiLogOut />
+            Logout
+          </MenuItem>
+        </Menu>
       </div>
     </div>
   );
