33ae75df529a688aa69296d0fbfaad32c60af395
Janel Semple <105823477+janelsemple@users.noreply.github.com>
2023-10-25 13:40:43 -0600

    Update index.tsx
    Removed comments.

--- app/components/Settings/SettingsModal/index.tsx
+++ app/components/Settings/SettingsModal/index.tsx
@@ -26,12 +26,6 @@ export default function ModalComponent({ open, onClose }: ModalComponentProps) {
 		<Modal open={open} onClose={onClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
 			<Box sx={modalStyle}>
 				<Profile />
-				{/*<Typography id="modal-modal-title" variant="h6" component="h2">*/}
-				{/*	Text in a modal*/}
-				{/*</Typography>*/}
-				{/*<Typography id="modal-modal-description" sx={{ mt: 2 }}>*/}
-				{/*	Duis mollis, est non commodo luctus, nisi erat porttitor ligula.*/}
-				{/*</Typography>*/}
 			</Box>
 		</Modal>
 	);
