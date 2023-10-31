cf93718a8884135c88797af0cbc1d0b1cbadd105
Caleb <calebneuf@gmail.com>
2023-10-31 12:48:04 -0600

    Cleaned up server.ts and app.ts

---
+++ app/routes/index.ts
@@ -0,0 +1,12 @@
+import {Router} from 'express';
+import audioRoutes from './audioRoutes';
+import transcriptionRoutes from './transcriptionRoutes';
+
+/**
+ * Router for all API endpoints.
+ */
+const router = Router();
+router.use('/audio', audioRoutes);
+router.use('/transcription', transcriptionRoutes);
+
+export default router;
