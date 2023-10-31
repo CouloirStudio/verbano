e1ab117363be8b3453a552345ecb38974a364c20
Janel Semple <105823477+janelsemple@users.noreply.github.com>
2023-10-30 22:57:09 -0600

    Added a comment and removed refreshNoteDetails from the useEffect dependency array.

--- app/components/Audio/AudioHeader/AudioHeader.tsx
+++ app/components/Audio/AudioHeader/AudioHeader.tsx
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { useProjectContext } from '@/app/contexts/ProjectContext';
-import Box from '@mui/material/Box';
-import styles from './audio.module.scss';
-import Recorder from '@/app/components/Audio/Recorder';
-import { Playback } from '@/app/components/Audio/Playback';
-import { TranscriptionButton } from '@/app/components/Audio/Transcription';
-import { useNoteContext } from '@/app/contexts/NoteContext';
-import { useTheme } from '@mui/material/styles';
+import React, { useEffect, useState } from "react";
+import { useProjectContext } from "@/app/contexts/ProjectContext";
+import Box from "@mui/material/Box";
+import styles from "./audio.module.scss";
+import Recorder from "@/app/components/Audio/Recorder";
+import { Playback } from "@/app/components/Audio/Playback";
+import { TranscriptionButton } from "@/app/components/Audio/Transcription";
+import { useNoteContext } from "@/app/contexts/NoteContext";
+import { useTheme } from "@mui/material/styles";
 
+/**
+ * A component that houses the three main audio components.
+ * @constructor
+ */
 const AudioHeader = () => {
   const { selectedNote } = useProjectContext();
   const { refreshNoteDetails } = useNoteContext();
   const [hasRecording, setHasRecording] = useState(false);
   const theme = useTheme();
 
-  // add function to do proper checking for audio location with error handling, then modify to be a proper functional component
-
+  // Component updates when selected note is changed.
   useEffect(() => {
     setHasRecording(!!(selectedNote && selectedNote.audioLocation));
-  }, [selectedNote, refreshNoteDetails]);
+  }, [selectedNote]);
 
   return (
     <Box
