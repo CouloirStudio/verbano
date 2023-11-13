4b78eaa200d008474b8f744d8f2c8f04a0fc4d80
Adrian <Adrian.tomin@gmail.com>
2023-11-13 15:44:51 -0700

    Updated uri to use https

--- app/config/apolloClient.ts
+++ app/config/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:3000/graphql',
+  uri: 'https://localhost:3000/graphql',
   credentials: 'include',
 });
 
