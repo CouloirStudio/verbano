2732d8bad4717238a02a45b0c427ee528bf42104
Caleb <calebneuf@gmail.com>
2023-12-05 18:47:23 -0700

    GPT model

--- app/services/OpenAIService.ts
+++ app/services/OpenAIService.ts
@@ -62,7 +62,7 @@ class OpenAIService {
       const combinedNotes = notes.map((note) => note.transcription).join('\n');
 
       const completion = await this.openai.chat.completions.create({
-        model: 'gpt-3.5-turbo-1106',
+        model: 'gpt-4-1106-preview',
         messages: [
           { role: 'user', content: combinedNotes },
           {
